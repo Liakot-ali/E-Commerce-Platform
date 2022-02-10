c7c18fcffe206a83c9e975b1cd74ccef51bee5d5
Liakot-ali <liakotali2k18@gmail.com>
2022-02-10 21:32:06 +0600

    Colors ans dimen added

--- app/src/main/java/com/team12/MainActivity.java
+++ app/src/main/java/com/team12/MainActivity.java
@@ -2,13 +2,24 @@ package com.team12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        home.findViewById(R.id.homeButton);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ActivityHome.class));
+            }
+        });
     }
 }
