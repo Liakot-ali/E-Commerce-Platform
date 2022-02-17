d720003ac07597f4949fbb6eba96663a67493f65
Sonu Kumar Keshari <kesharisonu9@gmail.com>
2022-02-17 03:29:17 -0800

    ActivityCustomerAddress, ActivityMyProfile, AcitivityNotification, ActivityUserProfile created and design by sonu

---
+++ app/src/main/java/com/team12/ActivityCustomerAddress.java
@@ -0,0 +1,14 @@
+package com.team12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityCustomerAddress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_address);
+    }
+}
