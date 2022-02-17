d4e3428ac2632199c8b368a52887bd9f07593b12
jannati99 <jannatulferdousehstu@gmail.com>
2022-02-18 00:29:58 +0600

    post product and product for buy add

---
+++ app/src/main/java/com/team12/post_product_details.java
@@ -0,0 +1,14 @@
+package com.team12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class post_product_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_product_details);
+    }
+}
