392a1aee9f8554a1291085917f0496dd9c62ca12
Liakot-ali <liakotali2k18@gmail.com>
2022-04-14 21:21:33 +0600

    seller application data store in admin section

--- app/src/main/java/com/team12/ActivitySellerApplicationForm.java
+++ app/src/main/java/com/team12/ActivitySellerApplicationForm.java
@@ -1,8 +1,10 @@
 package com.team12;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,14 +13,17 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 public class ActivitySellerApplicationForm extends AppCompatActivity {
 
     Toolbar toolbar;
-    EditText SellerName,SellerAddress,SellerPhone, SellerEmail,Description;
+    EditText SellerName, SellerAddress, SellerPhone, SellerEmail, Description;
     CheckBox checkBox;
     ImageView SellerPicture;
     Button Submit;
@@ -40,11 +45,58 @@ public class ActivitySellerApplicationForm extends AppCompatActivity {
     }
 
     private void OnClick() {
+
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Submit.setEnabled(checkBox.isChecked());
+            }
+        });
         Submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO---check the validity and store the value in Admin->SellerApproval->SellerId
                 Toast.makeText(ActivitySellerApplicationForm.this, "Under construction", Toast.LENGTH_SHORT).show();
+                name = SellerName.getText().toString();
+                phone = SellerPhone.getText().toString();
+                address = SellerAddress.getText().toString();
+                email = SellerEmail.getText().toString();
+                description = Description.getText().toString();
+
+
+                if (sellerId == 0) {
+                    sellerId = (long) (Math.random() * 1000000 + 1);
+                }
+
+                //-------store the application data to the admin section and change the sellerId in user Section-------
+                DatabaseReference adminRef = database.getReference("Admin").child("SellerApproval").child(String.valueOf(sellerId));
+                ClassSellerProfile sellerReq = new ClassSellerProfile(sellerId, name, null, phone, email, userId, address, description);
+                adminRef.setValue(sellerReq).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        DatabaseReference userRef = database.getReference("User").child(userId).child("Profile").child("sellerId");
+                        userRef.setValue(sellerId).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(ActivitySellerApplicationForm.this, "Your seller request is submitted to admin.", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(ActivitySellerApplicationForm.this, ActivityHome.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    finish();
+                                    SellerName.setText("");
+                                    SellerEmail.setText("");
+                                    SellerPhone.setText("");
+                                    SellerAddress.setText("");
+                                    Description.setText("");
+                                    SellerPicture.setImageResource(R.drawable.ic_demo_profile_picture_24);
+
+                                }else{
+                                    Toast.makeText(ActivitySellerApplicationForm.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                });
             }
         });
     }
@@ -78,10 +130,12 @@ public class ActivitySellerApplicationForm extends AppCompatActivity {
         SellerEmail.setText(email);
         SellerAddress.setText(address);
         Description.setText(description);
-        if(picture != null){
+        if (picture != null) {
             Picasso.get().load(picture).into(SellerPicture);
-        }else {
+        } else {
             SellerPicture.setImageResource(R.drawable.ic_demo_profile_picture_24);
         }
+
+        Submit.setEnabled(checkBox.isChecked());
     }
 }
