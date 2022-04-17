5a04eac92c68a781af8bd49be28e82b47fdd476a
Liakot-ali <liakotali2k18@gmail.com>
2022-04-17 21:14:41 +0600

    item find in java class and setOnClickListner

--- app/src/main/java/com/team12/AdminHome.java
+++ app/src/main/java/com/team12/AdminHome.java
@@ -2,9 +2,12 @@ package com.team12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class AdminHome extends AppCompatActivity {
     ImageView product, seller, sellerList, feedback;
@@ -15,19 +18,51 @@ public class AdminHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
         Initialize();
+        OnClick();
+    }
+
+    private void OnClick() {
+        product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminHome.this, AdminApproveProduct.class);
+                startActivity(intent);
+            }
+        });
+
+        seller.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AdminHome.this, "Under Construction", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        sellerList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AdminHome.this, "Under Construction", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AdminHome.this, "Under Construction", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void Initialize() {
 
         product = findViewById(R.id.adminHomeProduct);
-        product = findViewById(R.id.adminHomeProduct);
-        product = findViewById(R.id.adminHomeProduct);
-        product = findViewById(R.id.adminHomeProduct);
+        seller = findViewById(R.id.adminHomeSeller);
+        sellerList = findViewById(R.id.adminHomeSellerList);
+        feedback = findViewById(R.id.adminHomeFeedback);
 
-        product = findViewById(R.id.adminHomeProduct);
-        product = findViewById(R.id.adminHomeProduct);
-        product = findViewById(R.id.adminHomeProduct);
-        product = findViewById(R.id.adminHomeProduct);
+        productLay = findViewById(R.id.adminHomeProductLayout);
+        sellerLay= findViewById(R.id.adminHomeSellerLayout);
+        sellerListLay = findViewById(R.id.adminHomeSellerListLayout);
+        feedbackLay = findViewById(R.id.adminHomeFeedbackLayout);
 
     }
 }
